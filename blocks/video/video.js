59a3c10d6ba7919db768cceb7e68d0b01b0b074d
Szymon <sznajder.szymon@gmail.com>
2024-07-04 00:42:04 +0200

    update video

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -13,7 +13,7 @@ function embedYoutube(url, autoplay, background) {
     const suffixParams = {
       autoplay: autoplay ? '1' : '0',
       mute: background ? '1' : '0',
-      controls: background ? '0' : '1',
+      controls: background ? '1' : '0',
       disablekb: background ? '1' : '0',
       loop: background ? '1' : '0',
       playsinline: background ? '1' : '0',
